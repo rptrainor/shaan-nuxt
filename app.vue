859fc9479814d158123000661e17604e507d8a17
Ryan Trainor <rptrainor@gmail.com>
2024-05-09 19:09:37 -0300

    add hero section to home page

--- app.vue
+++ app.vue
@@ -1,12 +1,26 @@
 <template>
-	<main class="flex flex-col gap-4 bg-background-primary h-screen p-4">
-		<p>Hey, I'm Shaan&#8230;</p>
-		<h1 class="text-7xl uppercase font-medium text-text-primary font-sans">I WRITE THINGS HERE.</h1>
+	<main class="h-screen bg-background-primary p-5">
+		<div class="flex flex-col gap-2 max-w-7xl mx-auto">
+			<p
+				class="relative -left-4 top-2 sm:top-3 max-w-fit -rotate-6 rounded bg-background-accent px-3 py-2 text-lg font-semibold leading-none">
+				Hey, I'm Shaan&#8230;
+			</p>
+			<h1 class="text-balance font-sans text-4xl font-medium uppercase text-text-primary md:text-5xl lg:text-7xl">
+				I WRITE THINGS HERE.
+			</h1>
+			<p class="paragraph-1-125 u-mb-0">Join&nbsp;<span class="font-semibold">109,050</span>&nbsp;entrepreneurs on my
+				email list.
+				It's good, I&nbsp;swear.</p>
+			<iframe title="newsletter" src="https://embeds.beehiiv.com/0981ff0c-788f-4a28-937e-59be0cc398f5?slim=true"
+				data-test-id="beehiiv-embed" height="52" frameborder="0" class="w-full text-xs max-w-md"
+				style="margin: 0; border-radius: 0px !important; background-color: transparent;"></iframe>
+		</div>
 	</main>
 </template>
 
 <style scoped>
-h1, p {
-  font-family: 'PPNeueMontreal', sans-serif;
+h1,
+p {
+	font-family: 'PPNeueMontreal', sans-serif;
 }
 </style>
